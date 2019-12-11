ad407610a731a336ce76184a82d49373bbdb831d
DennySPB <dennyspb@gmail.com>
2019-12-11 09:08:53 +0300

    Revert "kernel: Boost CPU and DDR bus to the max when an app is launched"
    This reverts commit 197790205287a6cf717e6e1f6e398a674205f203.
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -81,8 +81,6 @@
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
 #include <linux/simple_lmk.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2112,12 +2110,6 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-
-	/* Boost to the max for 1250 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(1250);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1250);
-	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
